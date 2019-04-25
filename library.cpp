e7c97b61aa0a011212c6da4c66dd7cf2d6e5892d
nipunsingal <500069052@stu.upes.ac.in>
2019-04-25 19:15:05 +0530

    funct to delete record of file added

--- library.cpp
+++ library.cpp
@@ -269,3 +269,67 @@ void modify_student()
         cout<<"\n\n Record Not Found ";
     getch();
 }
+
+//***************************************************************
+//        function to delete record of file
+//****************************************************************
+
+
+void delete_student()
+{
+    char n[6];
+    int flag=0;
+    clrscr();
+        cout<<"\n\n\n\tDELETE STUDENT...";
+        cout<<"\n\nEnter The admission no. of the Student You Want To Delete : ";
+        cin>>n;
+        fp.open("student.dat",ios::in|ios::out);
+        fstream fp2;
+        fp2.open("Temp.dat",ios::out);
+        fp.seekg(0,ios::beg);
+        while(fp.read((char*)&st,sizeof(student)))
+    {
+        if(strcmpi(st.retadmno(),n)!=0)
+                 fp2.write((char*)&st,sizeof(student));
+        else
+               flag=1;
+    }
+
+    fp2.close();
+        fp.close();
+        remove("student.dat");
+        rename("Temp.dat","student.dat");
+        if(flag==1)
+             cout<<"\n\n\tRecord Deleted ..";
+        else
+             cout<<"\n\nRecord not found";
+        getch();
+}
+
+
+void delete_book()
+{
+    char n[6];
+    clrscr();
+    cout<<"\n\n\n\tDELETE BOOK ...";
+    cout<<"\n\nEnter The Book no. of the Book You Want To Delete : ";
+    cin>>n;
+    fp.open("book.dat",ios::in|ios::out);
+    fstream fp2;
+    fp2.open("Temp.dat",ios::out);
+    fp.seekg(0,ios::beg);
+    while(fp.read((char*)&bk,sizeof(book)))
+    {
+        if(strcmpi(bk.retbno(),n)!=0)
+        {
+            fp2.write((char*)&bk,sizeof(book));
+        }
+    }
+
+    fp2.close();
+        fp.close();
+        remove("book.dat");
+        rename("Temp.dat","book.dat");
+        cout<<"\n\n\tRecord Deleted ..";
+        getch();
+}
