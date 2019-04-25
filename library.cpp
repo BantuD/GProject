6809eb865f83a1dbaaa4bef0e137e560925c7e49
nipunsingal <500069052@stu.upes.ac.in>
2019-04-25 19:11:04 +0530

    class student deleted and function to write in file added

--- library.cpp
+++ library.cpp
@@ -124,71 +124,43 @@ public:
 
 };         //class ends here
 
-class student
+//***************************************************************
+//        global declaration for stream object, object
+//****************************************************************
+
+fstream fp,fp1;
+book bk;
+student st;
+//***************************************************************
+//        function to write in file
+//****************************************************************
+
+void write_book()
 {
-    char admno[6];
-    char name[20];
-    char stbno[6];
-    int token;
-public:
-    void create_student()
+    char ch;
+    fp.open("book.dat",ios::out|ios::app);
+    do
     {
         clrscr();
-         cout<<"\nNEW STUDENT ENTRY...\n";
-        cout<<"\nEnter The admission no. ";
-        cin>>admno;
-        cout<<"\n\nEnter The Name of The Student ";
-        gets(name);
-        token=0;
-        stbno[0]='/0';
-        cout<<"\n\nStudent Record Created..";
-    }
+        bk.create_book();
+        fp.write((char*)&bk,sizeof(book));
+        cout<<"\n\nDo you want to add more record..(y/n?)";
+        cin>>ch;
+    }while(ch=='y'||ch=='Y');
+    fp.close();
+}
 
-    void show_student()
+void write_student()
+{
+    char ch;
+    fp.open("student.dat",ios::out|ios::app);
+    do
     {
-        cout<<"\nAdmission no. : "<<admno;
-        cout<<"\nStudent Name : ";
-        puts(name);
-        cout<<"\nNo of Book issued : "<<token;
-        if(token==1)
-            cout<<"\nBook No "<<stbno;
-    }
-
-    void modify_student()
-    {
-        cout<<"\nAdmission no. : "<<admno;
-        cout<<"\nModify Student Name : ";
-        gets(name);
-    }
-
-    char* retadmno()
-    {
-        return admno;
-    }
-
-    char* retstbno()
-    {
-        return stbno;
-    }
-
-    int rettoken()
-    {
-        return token;
-    }
-
-    void addtoken()
-    {token=1;}
-
-    void resettoken()
-    {token=0;}
-
-    void getstbno(char t[])
-    {
-        strcpy(stbno,t);
-    }
-
-    void report()
-    {cout<<"\t"<<admno<<setw(20)<<name<<setw(10)<<token<<endl;}
-
-};         //class ends here
+        st.create_student();
+        fp.write((char*)&st,sizeof(student));
+        cout<<"\n\ndo you want to add more record..(y/n?)";
+        cin>>ch;
+    }while(ch=='y'||ch=='Y');
+    fp.close();
+}
 
